5f139ec7eae88a17b290b07b206861ac2b98e54b
Paweł Koniarski <me@pkoniarski.com>
2017-02-27 17:43:12 +0100

    Add strava API wrapper dependency

--- mix.exs
+++ mix.exs
@@ -37,7 +37,10 @@ defmodule Brouteolli.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:strava, "~> 0.3.2"},
+     {:poison, "~> 3.1", override: true},
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
